a1436e97a034457ea342c4d80800e5173183cf33
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2021-09-21 00:25:34 +0100

    Rename heroku_build

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "heroku_app" "mebs" {
   sensitive_config_vars = merge(local.sensitive_config_vars.frontend, local.sensitive_config_vars.backend)
 }
 
-resource "heroku_build" "example" {
+resource "heroku_build" "app_build" {
   app        = heroku_app.mebs.name
   buildpacks = ["https://github.com/heroku/heroku-buildpack-nodejs#latest"]
 
